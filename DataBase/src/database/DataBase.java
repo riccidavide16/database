221d1231370df55548631e73504db005112d0f64
riccidavide16 <ricci.davide@franchettisalviani.net>
2026-04-29 09:21:33 +0200

    1° punto
    creare il database e contemporaneamente la connessione

--- DataBase/src/database/DataBase.java
+++ DataBase/src/database/DataBase.java
@@ -8,6 +8,9 @@ package database;
  *
  * @author ricci.davide
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DataBase {
 
     /**
@@ -15,6 +18,13 @@ public class DataBase {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:scuola.db");
+            System.out.println("Connessione riuscita!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
     
 }
